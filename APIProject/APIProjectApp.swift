9a37f717483236b75e2fed03961ddc09b94caae1
Joyce <s022466@students.lmsd.org>
2024-12-18 12:28:36 -0500

    Initial Commit

---
+++ APIProject/APIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIProjectApp.swift
+//  APIProject
+//
+//  Created by Joyce Chang (student LM) on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct APIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APIProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  APIProject
+//
+//  Created by Joyce Chang (student LM) on 12/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
